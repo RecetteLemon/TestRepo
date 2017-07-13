88e0e7f1a4af5ab1e1bd445ee9fd483a3cbede12
RecetteLemon <recette.lemon@gmail.com>
2017-07-13 12:54:24 +0900

    AStar

---
+++ AStar/mainGame.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "mainGame.h"
+
+
+mainGame::mainGame()
+{
+}
+
+
+mainGame::~mainGame()
+{
+}
+
+//초기화 해주는 함수
+HRESULT mainGame::init(void)
+{
+	gameNode::init(true);
+
+	SCENEMANAGER->addScene("길찾기씬", new aStarScene);
+
+	SCENEMANAGER->changeScene("길찾기씬");
+
+	return S_OK;
+}
+
+//메모리 해제 함수
+void mainGame::release(void)
+{
+	gameNode::release();
+
+
+}
+
+//연산해주는 곳
+void mainGame::update(void)
+{
+	gameNode::update();
+
+	SCENEMANAGER->update();
+	
+}
+
+//여기가 그려주는 곳
+void mainGame::render(void)
+{
+	PatBlt(getMemDC(), 0, 0, WINSIZEX, WINSIZEY, WHITENESS);
+	//==============================================================================
+	
+	SCENEMANAGER->render();
+	
+	//================================================================================
+	//건들지마라 이거도
+	this->getBackBuffer()->render(getHDC(), 0, 0);
+
+}
